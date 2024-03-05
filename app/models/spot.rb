bff704389290fe78e55f2d70cbf32db7fb66c381
Mathieu Rabet <rabet.mathieu@gmail.com>
2024-03-05 17:01:28 +0100

    added gem active storage & modified config files
    Signed-off-by: Mathieu Rabet <rabet.mathieu@gmail.com>

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -2,6 +2,7 @@ class Spot < ApplicationRecord
   has_many :reviews
   has_many :bookmarks
   has_many :photos
+  has_one_attached :photo
 
   DIFFICULTY = ['Facile', 'Modéré', 'Difficile']
   validates :difficulty, inclusion: { in: DIFFICULTY }
