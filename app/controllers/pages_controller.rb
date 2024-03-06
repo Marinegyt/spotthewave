5263bf165fa83ccb02668f989fdea6a1e089a0c1
Mathieu Rabet <rabet.mathieu@gmail.com>
2024-03-06 11:46:30 +0100

    create draft
    Signed-off-by: Mathieu Rabet <rabet.mathieu@gmail.com>

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :draft]
 
   def home
   end
+
+  def draft
+  end
+
 end
