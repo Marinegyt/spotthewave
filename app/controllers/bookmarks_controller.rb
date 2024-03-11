f06917281f8b72d2270cb2f1541d3b0bbeca46b1
Perrine Guerrapin <perrine.guerrapin@gmail.com>
2024-03-11 16:30:32 +0100

    créé liste de mes favoris ok

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,20 @@
 class BookmarksController < ApplicationController
+  # before_action :set_spot, only: %i[new create]
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark.spot = @spot
+    @bookmark.save
+    redirect_to spot_path(@spot)
+  end
+
+  private
+
+  def set_spot
+    @spot = Spot.find(params[:spot_id])
+  end
+
 end
