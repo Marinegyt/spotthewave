ac7dce1e46dde6a3d666c0cad1a0103dc516525f
Mathieu Rabet <rabet.mathieu@gmail.com>
2024-03-12 10:11:47 +0100

    did some changes to the show
    Signed-off-by: Mathieu Rabet <rabet.mathieu@gmail.com>

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -34,29 +34,29 @@ class SpotsController < ApplicationController
     @average_rating = Review.average_rating(@spot)
 
     # @water_infos = WeatherService.call_water_weather(@spot.latitude, @spot.longitude)
-    @days = call_weather(@spot.latitude, @spot.longitude)
+    # @days = call_weather(@spot.latitude, @spot.longitude)
   end
 
   private
 
-  def call_weather(latitude,longitude)
-    url = "http://api.weatherapi.com/v1/forecast.json"
-    params = {
-      q: "#{latitude},#{longitude}",
-      days: 7
-    }
-    key = { key: ENV["WEATHER_API_KEY"] }
-    response = Faraday.get(url, params, key)
-    parsed_response = JSON.parse(response.body)
+  # def call_weather(latitude,longitude)
+  #   url = "http://api.weatherapi.com/v1/forecast.json"
+  #   params = {
+  #     q: "#{latitude},#{longitude}",
+  #     days: 7
+  #   }
+  #   key = { key: ENV["WEATHER_API_KEY"] }
+  #   response = Faraday.get(url, params, key)
+  #   parsed_response = JSON.parse(response.body)
 
-    my_days = []
-    parsed_response["forecast"]["forecastday"].each do |day|
-      my_days << day["hour"]
-    end
-    my_days
-  end
+  #   my_days = []
+  #   parsed_response["forecast"]["forecastday"].each do |day|
+  #     my_days << day["hour"]
+  #   end
+  #   my_days
+  # end
 
-  def days
-    call_weather(latitude,longitude)
-  end
+  # def days
+  #   call_weather(latitude,longitude)
+  # end
 end
