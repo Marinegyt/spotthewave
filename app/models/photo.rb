b728206f057449491d10d28980298c6ce69c111a
Cedric Mingam <mingam.cedric@gmail.com>
2024-03-12 17:44:25 +0100

    first seeds & gallery done

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,9 +1,9 @@
 class Photo < ApplicationRecord
   belongs_to :spot
   belongs_to :user
-  has_one_attached :image
+  has_many_attached :images
 
-  def feed_content
-    return { image_tag: image.key }
-  end
+  # def feed_content
+  #   return { image_tag: image.key }
+  # end
 end
