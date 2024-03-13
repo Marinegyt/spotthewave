e0fd97853f1b652ee01485131f317150efea159c
Mathieu Rabet <rabet.mathieu@gmail.com>
2024-03-13 11:54:52 +0100

    Add action cable for feed, fix seed for user_melanie
    Signed-off-by: Mathieu Rabet <rabet.mathieu@gmail.com>

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -10,6 +10,9 @@ class ReviewsController < ApplicationController
     @review.spot = @spot
     @review.user = current_user
     if @review.save
+
+      broadcast_to_subscribed_users
+
       redirect_to spot_path(@spot)
     else
       flash.now[:alert] = @review.errors.full_messages.join(', ')
@@ -22,6 +25,21 @@ class ReviewsController < ApplicationController
   def review_params
     params.require(:review).permit(:content, :rate, :difficulty)
   end
+
+  def broadcast_to_subscribed_users
+    # Find all users subscribed to the spot
+    users = @spot.users
+
+    # For each of these users
+    users.each do |user|
+      # Broadcast the review to their feed
+      FeedChannel.broadcast_to(
+        user,
+        render_to_string(partial: "shared/feed_card_review", locals: { instance: @review })
+      )
+    end
+  end
+
   # def new
   #   @review = Review.new
   #   authorize @review
