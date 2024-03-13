5d0e966c43fcc681e7199136d6fe6e01b1ecdfe5
Perrine Guerrapin <perrine.guerrapin@gmail.com>
2024-03-13 15:22:16 +0100

    navbar v1

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  # def show_svg(svg_name, classes = nil, data = nil)
+  #   svg = File.open("app/assets/images/icons/#{svg_name}.svg", "rb") { |file| raw file.read }
+  #   doc = Nokogiri::HTML::DocumentFragment.parse svg
+  #   svg = doc.at_css "svg"
+  #   svg["class"] = classes if classes.present?
+  #   data.each { |key, value| svg["data-#{key}"] = value } if data.present?
+  #   raw svg
+  # end
 end
