861cb17bcf669c9278917f8c7cf15804c3b59b30
Marine Gayet <146859421+Marinegyt@users.noreply.github.com>
2024-03-14 10:59:44 +0100

    Revert "Créé et fonctionnel - Map favoris et non favoris popup markers"

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -4,6 +4,7 @@ class BookmarksController < ApplicationController
     @spot = Spot.find(params[:spot_id])
     @bookmark = Bookmark.new(spot: @spot, user: current_user)
     if @bookmark.save
+      flash[:notice] = "Bien ajouté à vos favoris !"
       redirect_to spot_path(@spot)
     end
   end
@@ -12,6 +13,7 @@ class BookmarksController < ApplicationController
     @spot = Spot.find(params[:spot_id])
     @bookmark = Bookmark.find_by(spot: @spot, user: current_user)
     if @bookmark.destroy
+      flash[:notice] = "Bien supprimé de vos vos favoris !"
       redirect_to spot_path(@spot)
     end
   end
--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -19,7 +19,7 @@ class SpotsController < ApplicationController
         lat: spot.latitude,
         lng: spot.longitude,
         info_window_html: render_to_string(partial: "info_window", locals: {spot: spot}),
-        marker_html: current_user.bookmarks.map(&:spot).flatten.include?(spot) ? render_to_string(partial: "marker", locals: {spot: spot}) : render_to_string(partial: "marker_disabled", locals: {spot: spot})
+        marker_html: render_to_string(partial: "marker", locals: {spot: spot}),
       }
     end
 
